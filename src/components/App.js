4742aab9e5e82c948be3994e975cc4577ef7743b
Jean Urena <juanpedro1519@gmail.com>
2021-06-11 12:17:55 -0400

    Add React Routing and basic route for the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './Home';
 import ChampionsInfo from './ChampionsInfo';
 import ChampionsList from './ChampionsList';
 import ChampionListItem from './ChampionListItem';
@@ -5,11 +14,34 @@ import ChampionsFilters from './ChampionsFilters';
 
 function App(props) {
   return (
-    <div>
-      {/* <h1>Lapp</h1> */}
-      <ChampionsInfo />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/champions">Champions</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/champions">
+            <ChampionsInfo />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    // <div>
+    //   {/* <h1>Lapp</h1> */}
+    //   <ChampionsInfo />
       
-    </div>
+    // </div>
   );
 }
 
