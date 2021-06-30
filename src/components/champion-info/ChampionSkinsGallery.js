15e0765b0431b461ec1ec1af81c38d41ca0582d2
Jean Urena <juanpedro1519@gmail.com>
2021-06-30 12:49:25 -0400

    Champions skins image gallery using bootstrap carousel

--- src/components/champion-info/ChampionSkinsGallery.js
+++ src/components/champion-info/ChampionSkinsGallery.js
@@ -1,13 +1,34 @@
 import PropTypes, { object } from 'prop-types';
+import Carousel from 'react-bootstrap/Carousel';
 import ChampionAvatar from '../ChampionAvatar';
 import { DATADRAGON_VERSIONED_IMAGES_URL, DATADRAGON_IMAGES_URL} from '../../api/APIUtils';
 
 function ChampionSkinsGallery(props) {
   const { championData } = props;
+
   return (
     <div className="champion-skins-gallery">
-      <h2>Skins:</h2>
-        {championData.skins.map(skin => {
+      <h2 className="champions-skins-gallery__title">Available Skins ({championData.skins.length}):</h2>
+      <div className="champions-skins-gallery__carousel-container">
+        <Carousel interval={null}>
+          {championData.skins.map(skin => {
+            return (
+              <Carousel.Item>
+                <img 
+                  className='full-width-image'
+                  src={`${DATADRAGON_IMAGES_URL}\\${championData.image.group}\\splash\\${championData.id}_${skin.num}.jpg`} 
+                  alt={skin.name}
+                />
+                <Carousel.Caption>
+                  <h3 className="carousel-caption-text">{skin.name}</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      </div>
+      
+        {/* {championData.skins.map(skin => {
           return (
             <>
               <p>{skin.name}</p>
@@ -19,7 +40,7 @@ function ChampionSkinsGallery(props) {
               />
             </>
           );
-        })}  
+        })}   */}
     </div>
   );
 }
